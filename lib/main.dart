4b8243ddff6216e0f239027cd78a62736e681b89
hughesdev <94917862+hughesdev@users.noreply.github.com>
2021-11-29 12:09:17 -0700

    commit on 'master'

--- lib/main.dart
+++ lib/main.dart
@@ -40,23 +40,43 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: 
-      Center(
-        child: ListTile(
-          leading: const CircleAvatar(
-            radius: 30,
-            backgroundImage: AssetImage("assets/images/profile photo.jpg") 
-          ),
-          title: const Text("Ghana Telecom"),
-          subtitle: Row(
-            children: [const Icon(Icons.done_all),
-            SizedBox(width: 5,),
-             const Text("yes please")],
-          ),
-          trailing: Column(
-            children: [Text("5:30"), Icon(Icons.notifications)],
-          ) ,
-        )
-        )  
+      Column(
+        children: [
+           ListTile(
+              leading: const CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage("assets/images/profile photo.jpg") 
+              ),
+              title: const Text("Ghana Telecom"),
+              subtitle: Row(
+                children: [const Icon(Icons.done_all),
+                SizedBox(width: 5,),
+                 const Text("yes please")],
+              ),
+              trailing: Column(
+                children: [Text("5:30"), Icon(Icons.notifications)],
+              ) ,
+            ),
+          Card(
+            color: Colors.blue,
+            child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage("https://images.unsplash.com/photo-1589939705384-5185137a7f0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80") 
+                ),
+                title: const Text("Duapa Werkspace"),
+                subtitle: Row(
+                  children: [
+                  SizedBox(width: 5,),
+                   const Text("we have a contract for you")],
+                ),
+                trailing: Column(
+                  children: [Text("5:39"), CircleAvatar(radius: 10, backgroundColor: Colors.green,child: Text("4"),)],
+                ) ,
+              ),
+          )
+        ],
+      )  
     
       
       );
